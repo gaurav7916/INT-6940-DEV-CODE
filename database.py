ffe7f9e99a6154911751537d8531d426e91c4e1e
Gaurav Gupta <65064519+gaurav7916@users.noreply.github.com>
2025-11-13 14:54:51 -0500

    Refactor database connection configuration
    Updated DATABASE_URL to use Config variables for better configuration management.

--- database.py
+++ database.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
+# import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
+DATABASE_URL = f"postgresql://{Config.DB_USER}:{Config.DB_PASSWORD}@{Config.DB_HOST}:{Config.DB_PORT}/{Config.DB_NAME}"
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://myuser:password@localhost:5432/postgres")
+# use it for local testing
+# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://myuser:password@localhost:5432/postgres")
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
